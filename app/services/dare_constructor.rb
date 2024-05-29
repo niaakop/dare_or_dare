0662cc9800bfc26e17b54c58db55ff981e166eab
niaakop <agniakopova+FAKE@gmail.com>
2024-05-29 20:14:11 +0400

    Replace player_ids with players, remove same_gender and opposite_gender methods.

--- app/services/dare_constructor.rb
+++ app/services/dare_constructor.rb
@@ -1,32 +1,36 @@
+# Usage example:
+# game = Game.find_by(id:1),
+# template_1 = "Hey {target}, do smthg to {female}. Then do it with {male} and {opposite}. At last, do anythig to {same} and {any}."
+# dc_1 = DareConstructor.new(template_1, game)
+# dc_1.construct_dare
+
 class DareConstructor
   def initialize(template, game)
     @template = template
     @game = game
-    @players_ids = []
+    @players = []
   end
 
   def construct_dare
-    @players_ids = @game.players.pluck(:id)
-    @players_ids.delete(current_player.id)
+    @players = @game.players.where.not(id: current_player.id).to_a
     dare = @template.dup
     dare.gsub!("{target}", current_player.name)
-    dare = replace_players(dare, "{female}", female_players_ids)
-    dare = replace_players(dare, "{male}", male_players_ids)
-    dare = replace_players(dare, "{opposite}", opposite_gender_players_ids)
-    dare = replace_players(dare, "{same}", same_gender_players_ids)
-    dare = replace_players(dare, "{any}", @players_ids)
+    dare = replace_players(dare, "{female}", female_players)
+    dare = replace_players(dare, "{male}", male_players)
+    dare = replace_players(dare, "{opposite}", current_player.gender == 'Female' ? male_players : female_players)
+    dare = replace_players(dare, "{same}", current_player.gender == 'Male' ? male_players : female_players)
+    dare = replace_players(dare, "{any}", @players)
     dare
   end
 
   private
 
-  def replace_players(dare, placeholder, player_ids)
-    while dare.include?(placeholder) && player_ids.any?
-      player_id = player_ids.sample
-      player = Player.find(player_id)
+  def replace_players(dare, placeholder, players)
+    while dare.include?(placeholder) && players.any?
+      player = players.sample
       dare.sub!(placeholder, player.name)
-      @players_ids.delete(player_id)
-      player_ids.delete(player_id)
+      @players.delete(player)
+      players.delete(player)
     end
     dare
   end
@@ -35,36 +39,11 @@ class DareConstructor
     @game.current_player
   end
 
-  def same_gender_players_ids
-    gender = current_player.gender
-    @players_ids.select { |id| player = Player.find(id); player.gender == gender}
+  def female_players
+    @players.select { |player| player.gender == 'Female' }
   end
 
-  def opposite_gender_players_ids
-    gender = current_player.gender
-    @players_ids.select { |id| player = Player.find(id); player.gender != gender}
-  end
-
-  def female_players_ids
-    @players_ids.select do |id| 
-      player = Player.find(id)
-      player.gender == 'Female'
-    end
-  end
-
-  def male_players_ids
-    @players_ids.select do |id| 
-      player = Player.find(id)
-      player.gender == 'Male'
-    end
+  def male_players
+    @players.select { |player| player.gender == 'Male' }
   end
 end
-
-# Usage example:
-# template_1 = "Hey {target}, do smthg to {female}. Then do it with {male} and {opposite}. At last, do anythig to {same} and {any}."
-# template_2 = "Hey {target}! Do something to {female} and {female}. And to {same} and {same}."
-# game = Game.find_by(id: 1)
-# dc_1 = DareConstructor.new(template_1, game)
-# dc_2 = DareConstructor.new(template_2, game)
-# dc_1.construct_dare
-# dc_2.construct_dare
